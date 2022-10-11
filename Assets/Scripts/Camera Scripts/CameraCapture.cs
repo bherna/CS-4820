7821fd1acfc8a76aa777586a89c7f931b7013c98
bherna <bherna153@gmail.com>
2022-10-11 00:52:02 -0600

    we can  now have grey scale of the picture taken

--- Assets/Scripts/Camera Scripts/CameraCapture.cs
+++ Assets/Scripts/Camera Scripts/CameraCapture.cs
@@ -18,6 +18,7 @@
      public int fileCounter = 0;
      public string cameraAngle = "";
  
+
      void Start()
      {
          OVcamera = gameObject;
@@ -52,13 +53,67 @@
         imageOverview.Apply();
         RenderTexture.active = currentRT;    
         
-        // Encode texture into PNG
-        byte[] bytes = imageOverview.EncodeToJPG();
-        Destroy(imageOverview);
+        //save copy in color
+        saveImage(imageOverview);
 
-        // save in memory
-        //make sure the path exsits first
-        File.WriteAllBytes(Application.dataPath + "/Backgrounds/" + cameraAngle + fileCounter + ".jpg", bytes);
-        fileCounter++;
+        //convert to greyscale
+        imageOverview = convertToGrey(imageOverview);
+
+        //save copy in greyscale
+        saveImage(imageOverview);
+
+
      }
+
+
+
+    //save image to computer
+    private void saveImage(Texture2D imageOverview){
+
+        byte[] bytes = imageOverview.EncodeToJPG();
+        //Destroy(imageOverview);
+        String filename = cameraAngle + fileCounter + ".jpg";
+        File.WriteAllBytes(Application.dataPath + "/Backgrounds/" + filename, bytes);
+        fileCounter++;
+    }
+
+
+
+    //convet texture to greyscale
+    private Texture2D convertToGrey(Texture2D graph){
+        
+        Texture2D grayImg;
+
+        //convert texture
+        grayImg = new Texture2D(graph.width, graph.height, graph.format, false);
+        Graphics.CopyTexture(graph, grayImg);
+        Color32[] pixels = grayImg.GetPixels32();
+        Color32[] changedPixels = new Color32[grayImg.width*grayImg.height];
+    
+        for (int x = 0; x < grayImg.width; x++)
+        {
+            for (int y = 0; y < grayImg.height; y++)
+            {
+                Color32 pixel = pixels[x + y * grayImg.width];
+                int p = ((256 * 256 + pixel.r) * 256 + pixel.b) * 256 + pixel.g;
+                int b = p % 256;
+                p = Mathf.FloorToInt(p / 256);
+                int g = p % 256;
+                p = Mathf.FloorToInt(p / 256);
+                int r = p % 256;
+                float l = (0.2126f * r / 255f) + 0.7152f * (g / 255f) + 0.0722f * (b / 255f);
+                Color c = new Color(l, l, l, 1);
+                changedPixels[x + y * grayImg.width] = c;
+            }
+        }
+        grayImg.SetPixels32(changedPixels);
+        grayImg.Apply(false);
+        return grayImg;
+    }
+
+
+
+
+
+
  }
